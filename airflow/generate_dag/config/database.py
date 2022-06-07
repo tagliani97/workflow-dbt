35995734266c46b89defcb07921ec1bf49aef6a9
gatagliania <gatagliania.compasso@rd.com.br>
2022-06-07 12:19:22 -0300

    alteracao database

--- airflow/generate_dag/config/database.py
+++ airflow/generate_dag/config/database.py
@@ -1,16 +1,19 @@
 import psycopg2
+from airflow.hooks.base_hook import BaseHook  # Deprecated in Airflow 2
 
 
 class Database():
 
     @staticmethod
     def postgress_conection():
+
+        connection = BaseHook.get_connection("dbt_postgres_instance_raw_data")
         conn_args = dict(
-            host='172.18.0.3',
-            user='dbtuser',
-            password='pssd',
-            dbname='dbtdb',
-            port=5432
+            host=connection.host,
+            user=connection.login,
+            password=connection.password,
+            dbname=connection.schema,
+            port=connection.port
         )
         conn = psycopg2.connect(**conn_args)
         cur = conn.cursor()
